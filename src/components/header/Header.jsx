e7a44ad49b995e606f9e6efdca4e76458d0f663b
zidanislam <zidantwinkle@gmail.com>
2023-08-26 01:32:36 +0600

    hero section css complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,13 +4,13 @@ import "./header.css";
 
 const Header = () => {
   return (
-    <nav className="2xl:my-16 xl:my-10 flex justify-around">
+    <nav className="2xl:my-16 2xl:ml-80 xl:my-10 flex 2xl:justify-between xl:justify-around">
       <img
-        className="w-44"
+        className="w-48"
         src="https://i.ibb.co/RjzHvh3/DRESS.png"
         alt="logo"
       />
-      <div className="my-auto">
+      <div className="my-auto 2xl:mr-80">
         <Link to="/" className="2xl:mr-14 xl:mr-10 text-base">
           Home
         </Link>
@@ -24,12 +24,9 @@ const Header = () => {
           About
         </Link>
         <Link to="/contact" className="2xl:mr-14 xl:mr-10 text-base">
-          Contact
-        </Link>
-        <Link to="/contact" className="2xl:mr-14 xl:mr-10 text-base">
           Sign In
         </Link>
-        <Link to="/contact" className="2xl:mr-14 xl:mr-10 text-base">
+        <Link to="/contact" className="text-base">
           <button className="border rounded-xl bg-black text-white drop-shadow-2xl py-4 2xl:px-12 xl:px-10">
             Sign Up
           </button>
